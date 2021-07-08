f1af53f2fdf65be7e35b3d7c8e4afab252ee4847
Jakub Widurek <jakubwidurek@gmail.com>
2021-07-09 00:18:57 +0200

    Updates

---
+++ precache-manifest.69a6a6c1ac2b1efa588668ca1d6bd97d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "bccdb6a42e1cb225dc68",
+    "url": "/Simple-React-app-with-Fetch-Json/static/css/main.63d45470.chunk.css"
+  },
+  {
+    "revision": "bccdb6a42e1cb225dc68",
+    "url": "/Simple-React-app-with-Fetch-Json/static/js/main.bccdb6a4.chunk.js"
+  },
+  {
+    "revision": "4a34e45d34cafb7623bd",
+    "url": "/Simple-React-app-with-Fetch-Json/static/js/1.4a34e45d.chunk.js"
+  },
+  {
+    "revision": "1fceb67a0f017145289d",
+    "url": "/Simple-React-app-with-Fetch-Json/static/js/runtime~main.1fceb67a.js"
+  },
+  {
+    "revision": "330b7c0d278731aeafcceb10107599c0",
+    "url": "/Simple-React-app-with-Fetch-Json/index.html"
+  }
+];
